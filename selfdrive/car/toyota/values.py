56e8411adc3ca6ac9a1b11915e462dfd79b25fa0
wocsor <37704329+wocsor@users.noreply.github.com>
2019-01-17 19:41:58 -0500

    fix values.py

--- selfdrive/car/toyota/values.py
+++ selfdrive/car/toyota/values.py
@@ -163,7 +163,7 @@ DBC = {
   CAR.CAMRYH: dbc_dict('toyota_camry_hybrid_2018_pt_generated', 'toyota_prius_2017_adas'),
   CAR.HIGHLANDER: dbc_dict('toyota_highlander_2017_pt_generated', 'toyota_prius_2017_adas'),
   CAR.HIGHLANDERH: dbc_dict('toyota_highlander_hybrid_2018_pt_generated', 'toyota_prius_2017_adas'),
-  CAR.RAV4_2019: dbc_dict('toyota_rav4_2017_pt_generated', 'toyota_rav4_2019_adas')
+  CAR.RAV4_2019: dbc_dict('toyota_chr_2018_pt_generated', 'toyota_prius_2017_adas')
 }
 
 NO_DSU_CAR = [CAR.CHR, CAR.CHRH, CAR.CAMRY, CAR.CAMRYH, CAR.RAV4_2019]
